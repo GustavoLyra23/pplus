38f2bdaa9bad7402678e29c96a536a6d3cc992a0
GustavoLyra23 <gustavolyra23@gmail.com>
2025-10-12 17:20:10 -0300

    fix dumb syntax

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -45,7 +45,7 @@ fun executarArquivo(caminho: String) {
     }
 }
 
-/**TODO: pensar em uma forma melhor de usar essa funcao...
+/**TODO: pensar em uma melhor forma de usar essa funcao...
 //fun mostrarAjuda() {
 //    println("Uso do Portugol++ CLI:")
 //    println("  Sem argumentos   - Iniciar modo interativo REPL")
